cf3b2631336ea2b44d083240669f55d14cdc7b00
fus <fushengbx@163.com>
2016-05-07 15:06:24 +0800

    fix

--- app/build/bundle.js
+++ app/build/bundle.js
@@ -8459,21 +8459,65 @@ return /******/ (function(modules) { // webpackBootstrap
 
 var Mock = require('mockjs');
 
+var wWidth, wHeight;
+//生成随机数
+var _random = function _random(min, max) {
+	return min + Math.random() * (max - min + 1);
+};
+wWidth = document.body.clientWidth;
+wHeight = document.body.clientHeight;
 //mock
 var data = Mock.mock({
-	'list|24': [{
+	'list|1-24': [{
 		title: '@title',
-		image: '@image(240x400)'
+		image: '@image(240x400,@color)'
 	}]
 });
-console.log(data);
+for (var i = 0; i < data.list.length; i++) {
+	var item = data.list[i];
+	item.pos = {
+		top: _random(0, wHeight - 400) + 'px',
+		left: _random(0, wWidth - 240) + 'px',
+		transform: 'rotateZ(' + _random(0, 90) + 'deg)'
+	};
+}
+
+var ImgViewer = React.createClass({ displayName: "ImgViewer",
+	getInitialState: function getInitialState() {
+		return {
+			newStateClassName: 'front',
+			z: {
+				zIndex: 10
+			}
+		};
+	},
+	render: function render() {
+		return React.createElement("li", { className: this.state.newStateClassName,
+			onMouseOver: this.handleMouseOver, onMouseOut: this.handleMouseOut, style: $.extend(this.props.info.pos, this.state.z) }, React.createElement("img", { src: this.props.info.image, alt: this.props.info.title, title: this.props.info.title }), React.createElement("div", { className: "caption" }, React.createElement("h2", null, this.props.info.title)));
+	},
+	handleMouseOver: function handleMouseOver(e) {
+		this.setState({
+			newStateClassName: 'back',
+			z: {
+				zIndex: 20
+			}
+		});
+	},
+	handleMouseOut: function handleMouseOut(e) {
+		this.setState({
+			newStateClassName: 'front',
+			z: {
+				zIndex: 10
+			}
+		});
+	}
+});
 var Gallery = React.createClass({ displayName: "Gallery",
 	render: function render() {
 		var imageArray = [];
 		var createItem = function createItem(item) {
-			imageArray.push(React.createElement("li", null, React.createElement("img", { src: item.image, alt: item.title, title: item.title }), React.createElement("div", { className: "caption" }, React.createElement("h2", null, item.title))));
+			imageArray.push(React.createElement(ImgViewer, { info: item }));
 		};
-		console.log(this.props.list);
 		this.props.list.forEach(createItem);
 		return React.createElement("ul", { className: "gallery" }, imageArray);
 	}
--- app/src/jsx/Gallery.jsx
+++ app/src/jsx/Gallery.jsx
@@ -1,22 +1,74 @@
 
 var Mock = require('mockjs');
 
-
+var wWidth,wHeight;
+//生成随机数
+var _random=function(min,max){
+	return  min+Math.random()*(max-min+1);
+}
+wWidth=document.body.clientWidth;
+wHeight=document.body.clientHeight;
 //mock
 var data = Mock.mock({
-    'list|24': [{
+    'list|1-24': [{
         title: '@title',
-        image: '@image(240x400)'
+        image: '@image(240x400,@color)'
     }]
 });
-console.log(data)
+for (var i = 0; i < data.list.length; i++) {
+	let item=data.list[i];
+	item.pos={
+		top:_random(0,wHeight-400)+'px',
+		left:_random(0,wWidth-240)+'px',
+		transform:`rotateZ(${_random(0,90)}deg)`
+	}
+}
+
+var ImgViewer=React.createClass({
+	getInitialState() {
+	    return {
+	        newStateClassName:'front',
+	        z:{
+	        	zIndex:10
+	        }
+	    };
+	},
+	render(){
+		return (
+
+			<li className={this.state.newStateClassName} 
+				onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} style={$.extend(this.props.info.pos,this.state.z)}>
+					<img src={this.props.info.image} alt={this.props.info.title} title={this.props.info.title}/>
+					<div className="caption">	
+						<h2>{this.props.info.title}</h2>
+					</div>
+				</li>
+
+			)
+	},
+	handleMouseOver(e){
+		this.setState({
+			newStateClassName:'back',
+			z:{
+				zIndex:20
+			}
+		})
+	},
+	handleMouseOut(e){
+		this.setState({
+			newStateClassName:'front',
+			z:{
+				zIndex:10
+			}
+		})
+	}
+})
 var Gallery=React.createClass({
 	render(){
 		var imageArray=[];
 		var createItem=function(item){
-			imageArray.push(<li><img src={item.image} alt={item.title} title={item.title}/><div className="caption"><h2>{item.title}</h2></div></li>)
+			imageArray.push(<ImgViewer info={item}/>)
 		}
-		console.log(this.props.list)
 		this.props.list.forEach(createItem);
 		return (
 			<ul className="gallery">
